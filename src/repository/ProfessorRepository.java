c6df962d9753d466e3d2f03665ebd8ae31459142
MizunoYudi <joaomizunovy@gmail.com>
2025-11-22 09:06:46 -0300

    Criando repository de Professor

---
+++ src/repository/ProfessorRepository.java
@@ -0,0 +1,30 @@
+package repository;
+
+import model.Professor;
+import java.util.List;
+import java.util.ArrayList;
+
+public class ProfessorRepository {
+	private List<Professor> professores = new ArrayList<>();
+	
+	public void add(Professor prof) {
+		professores.add(prof);
+	}
+	
+	public void rmv(Professor prof) {
+		professores.remove(prof);
+	}
+	
+	public Professor getByMatricula(String matricula) {
+		for(Professor prof : professores) {
+			if(prof.getMatricula() == matricula) {
+				return prof;
+			}
+		}
+		return null;
+	}
+	
+	public List<Professor> listar(){
+		return professores;
+	}
+}

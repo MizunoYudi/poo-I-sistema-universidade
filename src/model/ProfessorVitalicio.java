0ab78003a73b4146c0e249dd4c2bd460189e2cca
MizunoYudi <joaomizunovy@gmail.com>
2025-11-22 08:24:20 -0300

    Criando classe Professor Vitalício na camada Model

---
+++ src/model/ProfessorVitalicio.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.time.LocalDate;
+
+public class ProfessorVitalicio extends Professor{	
+	public ProfessorVitalicio(String nome, String matricula, String titulacao, double salario, LocalDate dataContrato) {
+		super(nome, matricula, titulacao, salario, dataContrato);
+	}
+	
+	@Override
+	public double calcularSalario() {
+		double salario = getSalario();
+		
+		if(getTitulacao() == "DOUTORADO") {
+			salario += salario*20/100;
+		}
+		
+		return salario;
+	}
+}

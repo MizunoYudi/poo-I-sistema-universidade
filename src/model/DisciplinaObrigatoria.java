144f06a86ae6d3948d999797081178bcd5774eed
MizunoYudi <joaomizunovy@gmail.com>
2025-11-25 07:19:31 -0300

    Criando DisciplinaObrigatoria na camada Model

---
+++ src/model/DisciplinaObrigatoria.java
@@ -0,0 +1,8 @@
+package model;
+
+public class DisciplinaObrigatoria extends Disciplina{
+
+	public DisciplinaObrigatoria(String nome, int codigo, int cargaHoraria) {
+		super(nome, codigo, cargaHoraria);
+	}
+}

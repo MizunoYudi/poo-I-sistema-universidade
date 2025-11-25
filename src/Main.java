3cf4fbac5f11b92585f7210c0eb1300171b3a59a
MizunoYudi <joaomizunovy@gmail.com>
2025-11-25 19:44:29 -0300

    Movando main para a raiz de src

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ import controller.*;
 
 public class Main {
 	public static void main(String[] args) {
-		ProfessorRepository profRep = new ProfessorRepository();
-		ProfessorView profVw = new ProfessorView();
-		ProfessorController profCtr = new ProfessorController(profRep, profVw);
+		// ProfessorRepository profRep = new ProfessorRepository();
+		// ProfessorView profVw = new ProfessorView();
+		// ProfessorController profCtr = new ProfessorController(profRep, profVw);
 		AlunoRepository alRep = new AlunoRepository();
 		AlunoView alVw = new AlunoView();
 		AlunoController alCtr = new AlunoController(alRep, alVw);

7f1fae1d452576c34e03b484c5e67094e2fcd781
MizunoYudi <joaomizunovy@gmail.com>
2025-11-29 11:57:46 -0300

    Criando DisciplinasEletivas na camada Model

--- src/model/DisciplinaEletiva.java
+++ src/model/DisciplinaEletiva.java
@@ -3,9 +3,9 @@ package model;
 public class DisciplinaEletiva extends Disciplina{
 	private int interesse;
 
-	public DisciplinaEletiva(String nome, int codigo, int cargaHoraria, int interesse) {
+	public DisciplinaEletiva(String nome, int codigo, int cargaHoraria) {
 		super(nome, codigo, cargaHoraria);
-		this.interesse = interesse;
+		this.interesse = 0;
 	}
 
 	public int getInteresse() {

02712ea23242eeb60f629e8143dcd0693d4771d5
MizunoYudi <joaomizunovy@gmail.com>
2025-11-29 11:45:41 -0300

    Ligando Aluno a Disciplina

--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -1,8 +1,12 @@
 package model;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Aluno {
 	private String nome;
 	private String cpf;
+	private List<Disciplina> disciplinas = new ArrayList<>();
 	
 	public Aluno(String nome, String cpf) {
 		this.nome = nome;
@@ -23,5 +27,9 @@ public class Aluno {
 
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
+	}
+	
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
 	}
 }

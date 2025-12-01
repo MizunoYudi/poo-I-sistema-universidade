9ca4601257f50f8c03418dd6084114e5d59987a7
MizunoYudi <joaomizunovy@gmail.com>
2025-12-01 12:29:02 -0300

    Fazendo testes com disciplinas no Main

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,33 @@
-
-
 import view.*;
 import repository.*;
+import java.time.LocalDate;
 import controller.*;
+import model.*;
 
 public class Main {
 	public static void main(String[] args) {
-		// ProfessorRepository profRep = new ProfessorRepository();
-		// ProfessorView profVw = new ProfessorView();
-		// ProfessorController profCtr = new ProfessorController(profRep, profVw);
+		ProfessorRepository profRep = new ProfessorRepository();
+		ProfessorView profVw = new ProfessorView();
+		ProfessorController profCtr = new ProfessorController(profRep, profVw);
 		AlunoRepository alRep = new AlunoRepository();
 		AlunoView alVw = new AlunoView();
 		AlunoController alCtr = new AlunoController(alRep, alVw);
+		DisciplinaRepository dRep = new DisciplinaRepository();
+		DisciplinaView dVw = new DisciplinaView();
+		DisciplinaController dCtr = new DisciplinaController(dRep, dVw);
 		
-		alCtr.adicionarAluno(alVw.lerDadosAluno());
-		alCtr.adicionarAluno(alVw.lerDadosAluno());
-		alCtr.listarAlunos();
-		alCtr.removerAluno();
-		alCtr.listarAlunos();
+		profRep.add(new ProfessorVitalicio("Carlos", "abcd", "Doutorado", 2000, LocalDate.now()));
+		alRep.add(new Aluno("Pablo", "123345444"));
+		alRep.add(new Aluno("Pababa", "1asdadsa4"));
+		
+		dCtr.adicionarDisciplina(dVw.lerDadosDisciplinaObrigatoria(), profCtr.buscarPorMatricula());
+		dCtr.adicionarDisciplina(dVw.lerDadosDisciplinaEletiva(), profCtr.buscarPorMatricula());
+		dCtr.listarDisciplinas();
+		alCtr.adcionarDisciplina(dCtr.buscarDisciplinaPorCodigo());
+		alCtr.adcionarDisciplina(dCtr.buscarDisciplinaPorCodigo());
+		dCtr.listarAlunosDisciplina();
+		dCtr.removerDisciplina();
+		dCtr.listarDisciplinas();
+		
 	}
 }
